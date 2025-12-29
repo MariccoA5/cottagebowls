6dc4b4607fcd6d467671121626c8c0edee12e561
MELO <MariccoA5@gmail.com>
2025-12-29 16:26:19 -0700

    new look

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -1,3 +1,3 @@
 <template>
-  <img src="public/images/cottagebowls.JPG" alt="Cottage Bowls" class="h-10 md:h-12" />
+  <img src="/images/cottagebowls.JPG" alt="Cottage Bowls" class="h-10 md:h-12" />
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-sky-100">
     <UContainer class="py-20 text-center">
-      <img src="/images/logo.png" alt="Cottage Bowls" class="mx-auto h-48 mb-8" />
+      <img src="/images/cottagebowls.JPG" alt="Cottage Bowls" class="mx-auto h-48 mb-8" />
       <h1 class="text-5xl font-bold text-amber-600 mb-4">
         Fresh Cottage Bowls ☀️
       </h1>
